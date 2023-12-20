946a4c67fd6ecdefaeae302aafc86a6aa50d2f43
Jebin_3 <90751436+jebin232@users.noreply.github.com>
2023-12-20 16:13:17 +0530

    main.py

--- main.py
+++ main.py
@@ -1,38 +1,29 @@
 from fastapi import FastAPI, HTTPException
+from fastapi_limiter import FastAPILimiter
+from fastapi_limiter.depends import RateLimiterDependency
 from pytube import YouTube
 import logging
-import time
 
 app = FastAPI()
 
 logging.basicConfig(level=logging.ERROR)
 
-# Rate limiter variables
-MIN_REQUEST_INTERVAL = 2  # Minimum time between requests in seconds
-last_request_time = 0
+# Configure rate limiting
+limiter = FastAPILimiter(app, key_func=lambda _: "global", block=True)
+rate_limiter = RateLimiterDependency(limiter)
 
 @app.get("/download/")
-async def download_video(link: str):
-    global last_request_time
-
+@limiter.limit("5/minute")  # Adjust the rate limit as needed
+async def download_video(link: str, rate_limit: bool = Depends(rate_limiter)):
     try:
-        # Rate limiting
-        current_time = time.time()
-        elapsed_time = current_time - last_request_time
-        if elapsed_time < MIN_REQUEST_INTERVAL:
-            time.sleep(MIN_REQUEST_INTERVAL - elapsed_time)
-
         yt = YouTube(link)
         stream = yt.streams.get_highest_resolution()
         download_url = stream.url
-
-        # Update last request time
-        last_request_time = time.time()
-
         return {"download_link": download_url}
     except Exception as e:
         logging.error(f"Error processing request: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
+
 
 
 if __name__ == "__main__":
