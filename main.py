b17d00b2c87d55443411a02a66a7d98f07d1bab8
Jebin_3 <90751436+jebin232@users.noreply.github.com>
2023-12-25 12:07:48 +0530

    main.py

--- main.py
+++ main.py
@@ -1,43 +1,20 @@
-
 from fastapi import FastAPI, HTTPException
 from pytube import YouTube
-import logging
 import time
 
 app = FastAPI()
 
-logging.basicConfig(level=logging.ERROR)
-
-# Rate limiter variables
-MIN_REQUEST_INTERVAL = 2  # Minimum time between requests in seconds
-last_request_time = 0
-
 @app.get("/download/")
 async def download_video(link: str):
-    global last_request_time
-
     try:
-        # Rate limiting
-        current_time = time.time()
-        elapsed_time = current_time - last_request_time
-        if elapsed_time < MIN_REQUEST_INTERVAL:
-            time.sleep(MIN_REQUEST_INTERVAL - elapsed_time)
-
-        yt = YouTube(link)
+        yt = YouTube(f"https://youtu.be/ywvh1UHVjVg?si={link}")
         stream = yt.streams.get_highest_resolution()
         download_url = stream.url
-
-        # Update last request time
-        last_request_time = time.time()
-
         return {"download_link": download_url}
     except Exception as e:
-        logging.error(f"Error processing request: {e}")
-        raise HTTPException(status_code=500, detail="Internal Server Error")
-
-
-
+        return "no internet"
+        raise HTTPException(status_code=500, detail=str(e))
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
