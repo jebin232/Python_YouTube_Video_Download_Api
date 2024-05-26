86086fd0f35823cdeb837b34308a0f4fcda0ea55
Jebin_M <90751436+jebin232@users.noreply.github.com>
2024-05-26 12:29:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ from pytube import YouTube
 
 app = Flask(__name__)
 
-@app.route('/download', methods=['POST'])
+@app.route('/download/', methods=['POST'])
 def download_video():
-    data = request.json
-    if 'link' not in data:
-        return jsonify({"detail": "No link provided"}), 400
-
     try:
+        data = request.json
+        if not data or 'link' not in data:
+            print(f"Received data: {data}")  # Logging received data
+            return jsonify({"detail": "No link provided"}), 400
+
         yt = YouTube(data['link'])
         stream = yt.streams.get_highest_resolution()
         download_url = stream.url
         return jsonify({"download_link": download_url})
     except Exception as e:
+        print(f"Error: {e}")  # Logging error
         return jsonify({"detail": "Failed to process the video link."}), 400
 
 if __name__ == "__main__":
